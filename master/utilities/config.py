08da84d1579f1aa1b9ac6d15c4d9b8a372964e9a
Tyoma Gorbunov <bivaetctoskazat123@gmail.com>
2025-05-17 00:15:38 +0500

    fix: Маленькие правки относительно парсинга аргументов

--- master/utilities/config.py
+++ master/utilities/config.py
@@ -1,4 +1,5 @@
 ASCII_CHAR = list(' .",:;!~+-xmo*#W&8@')
+
 HTML_TEMPLATE = """<!DOCTYPE html>
 <html lang='en'>
 <head>
@@ -18,3 +19,19 @@ HTML_TEMPLATE = """<!DOCTYPE html>
 </body>
 </html>
 """
+
+ARGPARSE_DESCRIPTION = (
+    "Convert images to ASCII art. The program processes standard "
+    "image formats (JPG, PNG, etc.)\nby converting each pixel to "
+    "a corresponding ASCII character,\nproducing art that can be "
+    "saved as a text or HTML file."
+)
+
+ARGPARSE_EPILOG = (
+    "Examples:\n"
+    " python -m master.core.main ./image.jpg\n"
+    " python -m master.core.main ./image.jpg -sv ./output.txt\n"
+    " python -m master.core.main ./image.jpg -sv ./output.html -c\n"
+    " python -m master.core.main ./image.jpg -sv ./output.html -c -s 720 480\n"
+    " python -m master.core.main ./image.jpg -i\n"
+)
